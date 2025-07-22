22b50f27c3bb7e6a1ed668ce01197263a4876873
PRIYA <priyapillarisetty19@gmail.com>
2025-07-21 20:50:47 -0400

    Updated faqs api

--- dost/src/app/api/faqs/[id]/route.ts
+++ dost/src/app/api/faqs/[id]/route.ts
@@ -3,18 +3,15 @@ import { connectDB } from "@/lib/mongodb";
 import { Faq } from "@/models/Faq";
 import { NextRequest, NextResponse } from "next/server";
 
-type RouteContext = {
-  params: {
-    id: string;
-  };
-};
-
-export async function PUT(req: NextRequest, context: RouteContext) {
+export async function PUT(
+  req: NextRequest,
+  { params }: { params: { id: string } }
+) {
   await connectDB();
   const { question, answer } = await req.json();
 
   const updated = await Faq.findByIdAndUpdate(
-    context.params.id,
+    params.id,
     { question, answer },
     { new: true }
   );
@@ -22,8 +19,11 @@ export async function PUT(req: NextRequest, context: RouteContext) {
   return NextResponse.json(updated);
 }
 
-export async function DELETE(req: NextRequest, context: RouteContext) {
+export async function DELETE(
+  req: NextRequest,
+  { params }: { params: { id: string } }
+) {
   await connectDB();
-  await Faq.findByIdAndDelete(context.params.id);
+  await Faq.findByIdAndDelete(params.id);
   return NextResponse.json({ success: true });
 }
